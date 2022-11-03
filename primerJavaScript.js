b660558b25ce4642323c5f6d37509b5a5bdca850
Norma Portillo <norma.portillo8m@gmail.com>
2022-11-03 15:41:35 -0700

    API random User

--- primerJavaScript.js
+++ primerJavaScript.js
@@ -2,6 +2,14 @@ function funcionAlerta(){
     alert("le pique al click");
 }
 
+async function traePersona(){
+    const respuesta = await fetch("https://randomuser.me/api/");
+    //console.log(respuesta);
+    const datos = await respuesta.json();
+    //console.log(datos);
+    console.log(datos.results[0].name.last+" "+datos.results[0].name.first);
+}
+
 function quitarSITEC(){
     let pregunta = confirm("¿Seguro de quitar SITEC?");
     if(pregunta == true){
